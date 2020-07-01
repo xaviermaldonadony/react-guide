4a9a34b10d03934c0fb66867a09c5df1f75af62a
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-01 02:10:14 -0400

    Clean up work example in use effect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -17,20 +17,23 @@ const cockpit = (props) => {
 		// Similar to ComponentDidMount and
 		// componentDidUpdate
 		// Http request ...
-		setTimeout(() => {
+		const timer = setTimeout(() => {
 			alert('Saved data');
-		}, 1000);
+		}, 2000);
 		return () => {
+			clearTimeout(timer);
 			console.log('[cockpit.js] cleanup work in useEffect');
 		};
 	}, []); // wit that parameter it will run when rendered or unmounted
 
+	// this will run on every update cycle
 	useEffect(() => {
 		console.log('[Cockpit.js] 2nd useEffect []');
 		return () => {
 			console.log('[cockpit.js] cleanup work in 2nd  useEffect');
 		};
 	});
+
 	const assignedClasses = [];
 	let btnClass = '';
 
