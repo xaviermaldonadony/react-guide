b665e3152c57e47521688cba0767e87db1891ce9
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-02 22:55:44 -0400

    HOC WithClass ex

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.css';
 
@@ -6,7 +6,7 @@ class Person extends Component {
 	render() {
 		console.log('[Person.js] rendering...');
 		return (
-			<Fragment>
+			<Aux>
 				<p onClick={this.props.click}>
 					I'm {this.props.name} and I am {this.props.age} years old!
 				</p>
@@ -16,7 +16,7 @@ class Person extends Component {
 					onChange={this.props.changed}
 					value={this.props.name}
 				/>
-			</Fragment>
+			</Aux>
 		);
 	}
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import React, { useState, Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 	constructor(props) {
@@ -102,7 +103,7 @@ class App extends Component {
 		}
 
 		return (
-			<div className={classes.App}>
+			<WithClass classes={classes.App}>
 				<button
 					onClick={() => {
 						this.setState({ showCockpit: false });
@@ -119,7 +120,7 @@ class App extends Component {
 					/>
 				) : null}
 				{persons}
-			</div>
+			</WithClass>
 		);
 	}
 }
