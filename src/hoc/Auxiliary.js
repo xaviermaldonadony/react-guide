756318b57d3e3f023d9a031e9c8a433cdc0b8fea
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-02 22:26:21 -0400

    Adjacent JSX elements

---
+++ src/hoc/Auxiliary.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const aux = (props) => props.children;
+
+export default aux;
+
+// props.children, it is used to display whatever you include between the opening and closing tags when invoking a component.
