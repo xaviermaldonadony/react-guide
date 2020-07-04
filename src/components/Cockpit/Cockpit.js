6661fe1a8ea843606d8fb16fb5c0b1da9bb04242
xaviermaldonadony <xaviermaldonadony@hotmail.com>
2020-07-04 02:41:33 -0400

    contextType & useContext() - contextType, for classes that lets you get acces to context - useContext(), hook for func components - see Person class and Cockpit func component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import classes from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
@@ -18,6 +18,9 @@ const cockpit = (props) => {
 	// }, [props.persons]); // multiple fields
 
 	const toggleBtnRef = useRef(null);
+	const authContext = useContext(AuthContext);
+
+	console.log(authContext.authenticated);
 
 	useEffect(() => {
 		console.log('[Cockpit.js] useEffect []');
@@ -63,9 +66,7 @@ const cockpit = (props) => {
 			<button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
 				Toggle Persons
 			</button>
-			<AuthContext.Consumer>
-				{(context) => <button onClick={context.login}>Log in</button>}
-			</AuthContext.Consumer>
+			{<button onClick={authContext.login}>Log in</button>}
 		</div>
 	);
 };
